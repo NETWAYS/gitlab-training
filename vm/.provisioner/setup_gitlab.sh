9648b3eb187ee5d452873ea24de089fa0922e09c
Michael Friedrich <michael.friedrich@netways.de>
2017-10-17 17:51:52 +0200

    Add a Gitlab Vagrant playground for training screenshots

---
+++ vm/.provisioner/setup_gitlab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# https://about.gitlab.com/installation/?version=ce
+
+apt-get install -y curl openssh-server ca-certificates curl
+apt-get install -y vim
+update-alternatives --set editor /usr/bin/vim.basic
+
+echo postfix postfix/main_mailer_type select "Internet Site" | debconf-set-selections
+echo postfix postfix/mailname select `hostname -f` | debconf-set-selections
+echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections
+apt-get -y install language-pack-en postfix unattended-upgrades
+
+
+curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
+
+apt-get install -y gitlab-ce
+gitlab-ctl reconfigure
